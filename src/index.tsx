ff49fdfdc5135cb26ba5a491b025336681b8ea64
lxndrbukin <alex.bukin@hotmail.com>
2024-04-23 21:14:38 +0100

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+
+const rootDiv = document.querySelector('#root');
+
+if (rootDiv) {
+  const root = ReactDOM.createRoot(rootDiv);
+  root.render(<App />);
+}
---
+++ src/lib/index.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: "note-taker-bff39.firebaseapp.com",
+  projectId: "note-taker-bff39",
+  storageBucket: "note-taker-bff39.appspot.com",
+  messagingSenderId: "140162965206",
+  appId: "1:140162965206:web:c5f06ed52f5574ce7b3377",
+  measurementId: "G-694K8ZHLHL"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);

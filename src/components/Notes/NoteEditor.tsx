873dd3a29c2b95fd9d39a467988643b0dcfdf61d
lxndrbukin <alex.bukin@hotmail.com>
2024-04-27 18:09:27 +0100

    working on the Editor

--- src/components/Notes/NoteEditor.tsx
+++ src/components/Notes/NoteEditor.tsx
@@ -2,5 +2,10 @@ import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 export default function NoteEditor(): JSX.Element {
-  return <CKEditor editor={ClassicEditor} />;
+  return (
+    <CKEditor
+      onChange={(e, editor) => console.log(editor.getData())}
+      editor={ClassicEditor}
+    />
+  );
 }
